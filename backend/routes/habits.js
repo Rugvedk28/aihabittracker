591ec3b9baa725cba9c48cb02a2bf9c817e0e5a9
rugvedk28 <rugvedrk1189@gmail.com>
2026-06-29 18:05:40 +0530

    fixed a routing bug in habits.js

--- backend/routes/habits.js
+++ backend/routes/habits.js
@@ -1,16 +1,18 @@
 import express from "express";
-import { getHabits, createHabit, updateHabit, deleteHabit, reorderHabits  } from "../controllers/habitController.js";
+import { getHabits, createHabit, updateHabit, deleteHabit, archiveHabit, reorderHabits  } from "../controllers/habitController.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { archiveHabit } from "../controllers/habitController.js";
+// import { archiveHabit } from "../controllers/habitController.js";
 
 const router = express.Router();
 router.use(protect);
 
+// router.route("/").get(protect, getHabits).post(protect, createHabit);
+// router.route("/:id").put(protect, updateHabit,archiveHabit).delete(protect, deleteHabit, archiveHabit);
 router.get("/", getHabits);
-router.put("/reorder", reorderHabits);
-router.post("/", createHabit);
+router.post("/create", createHabit);
 router.put("/:id", updateHabit);
 router.delete("/:id", deleteHabit);
 router.put("/:id/archive", archiveHabit);
+router.put("/reorder", reorderHabits);
 
 export default router;
